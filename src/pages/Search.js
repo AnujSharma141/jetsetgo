6deb8ce70f86106efac0264c69d78128ac889a3a
anujsharma141 <19ucc109@lnmiit.ac.in>
2024-02-09 18:22:53 +0530

    update: responsive styles!

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -30,11 +30,11 @@ export default function Search(props) {
             <Nav />
             <div className='search-forms'>
                 <div className='search-locations'>
-                    <Input autocomplete="off" type="text" name="from" onChange={props.inputHandler} border='0' borderRadius='20vh 0 0 20vh' borderRight='1px solid #D1D1D1' p='2vw 2vw' placeholder='from' />
-                    <Input autocomplete="off" type="text" name="to" onChange={props.inputHandler} border='0' borderRadius='0vh 20vh 20vh 0vh' p='2vw 2vw' placeholder='destination' />
+                    <Input autocomplete="off" type="text" name="from" onChange={props.inputHandler} border='0' borderRadius='20vh 0 0 20vh' borderRight='1px solid #D1D1D1' p='4vh 4vh' placeholder='from' />
+                    <Input autocomplete="off" type="text" name="to" onChange={props.inputHandler} border='0' borderRadius='0vh 20vh 20vh 0vh' p='4vh 4vh' placeholder='destination' />
                 </div>
                 <div className='search-date'>
-                    <Input type="date" name="date" onChange={props.inputHandler}  border='0' borderRadius='20vh' p='2vw 2vw' id=""  />
+                    <Input type="date" name="date" onChange={props.inputHandler}  border='0' borderRadius='20vh' p='4vh 4vh' id=""  />
                 </div>
             </div>
         </div>
